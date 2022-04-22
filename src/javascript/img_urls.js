a209ebd7274036aad3070005e6f41cbee60653d7
codingBear <95178119+GDK01@users.noreply.github.com>
2022-04-22 11:59:00 +0900

    fin fe project

---
+++ src/javascript/img_urls.js
@@ -0,0 +1,37 @@
+export const mainPageImgs = [
+  { imgUrl: 'src/img/main_page/v_img01.jpg' },
+  { imgUrl: 'src/img/main_page/v_img02.jpg' },
+  { imgUrl: 'src/img/main_page/v_img03.jpg' },
+  { imgUrl: 'src/img/main_page/v_img04.jpg' },
+  { imgUrl: 'src/img/main_page/v_img05.jpg' },
+  { imgUrl: 'src/img/main_page/v_img06.jpg' },
+  { imgUrl: 'src/img/main_page/v_img07.jpg' },
+];
+
+export const sectionPageImgs = [
+  { imgUrl: 'src/img/section01.jpg' },
+  { imgUrl: 'src/img/section02.jpg' },
+  { imgUrl: 'src/img/section03.jpg' },
+  { imgUrl: 'src/img/section04.jpg' },
+];
+
+export const secOneIcons = [
+  { imgUrl: 'src/img/section01_icons/icon_sec1_1.png' },
+  { imgUrl: 'src/img/section01_icons/icon_sec1_2.png' },
+  { imgUrl: 'src/img/section01_icons/icon_sec1_3.png' },
+];
+
+export const secThreeIcons = [
+  { imgUrl: 'src/img/section03_icons/section3_01.png' },
+  { imgUrl: 'src/img/section03_icons/section3_02.png' },
+  { imgUrl: 'src/img/section03_icons/section3_03.png' },
+];
+
+export const secFourIcons = [
+  { imgUrl: 'src/img/section04_box/box1.jpg' },
+  { imgUrl: 'src/img/section04_box/box2.jpg' },
+  { imgUrl: 'src/img/section04_box/box3.jpg' },
+  { imgUrl: 'src/img/section04_box/box4.jpg' },
+  { imgUrl: 'src/img/section04_box/box5.jpg' },
+  { imgUrl: 'src/img/section04_box/box6.jpg' },
+];
